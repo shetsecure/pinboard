2e4f00d333bf3c1ba2dff61cc6f7eae55c08acaa
mouss <mouss@Lelouche>
2020-05-27 23:52:25 +0200

    adding images bonus

---
+++ src/pobj/pinboard/document/ClipImage.java
@@ -0,0 +1,36 @@
+package pobj.pinboard.document;
+
+import java.io.File;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
+public class ClipImage extends AbstractClip {
+
+	private Image image;
+	private File filename;
+
+	public  ClipImage(double left, double top, File filename){
+		super(left,top,0,0,new MyColor(Color.WHITE));
+		image = new Image("file://" + filename.getAbsolutePath());
+		double bottom = image.getHeight();
+		double right = image.getWidth();
+		setGeometry(left, top, right, bottom);
+		
+	}
+
+	public void draw(GraphicsContext ctx) {
+		ctx.drawImage(image, getLeft(), getTop());
+		
+	}
+
+	public ClipImage copy() {
+		return new ClipImage(getLeft(), getTop(), filename);
+	}
+	
+	public Image getImage() {
+		return image;
+	}
+
+}
